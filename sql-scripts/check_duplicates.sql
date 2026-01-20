37f6a65f03934908262215ba677f8e6a22efd8cb
Yugo l'Eliatrop <papasergiusv@gmail.com>
2026-01-20 17:13:22 +0100

    [FZR-04] detect duplicates

---
+++ sql-scripts/check_duplicates.sql
@@ -0,0 +1,19 @@
+WITH LaggedData AS (
+    SELECT 
+        id,
+        icao,
+        flight,
+        lat, 
+        lon,
+        updatedAt,
+        LAG(updatedAt) OVER (PARTITION BY icao ORDER BY updatedAt) as prev_time,
+        LAG(lat) OVER (PARTITION BY icao ORDER BY updatedAt) as prev_lat,
+        LAG(lon) OVER (PARTITION BY icao ORDER BY updatedAt) as prev_lon
+    FROM AircraftData
+)
+SELECT id, icao, updatedAt, lat, lon, flight
+FROM LaggedData
+WHERE 
+    lat = prev_lat 
+    AND lon = prev_lon
+    AND (updatedAt - prev_time) <= 240000;
